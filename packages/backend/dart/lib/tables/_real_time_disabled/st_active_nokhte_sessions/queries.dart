dba60766266141a93eedc4da56548a80e1d189a3
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-10-28 20:28:48 -0500

    refactor: pack enquiries to be more query centric instead of edge function based

--- packages/backend/dart/lib/tables/_real_time_disabled/st_active_nokhte_sessions/queries.dart
+++ packages/backend/dart/lib/tables/_real_time_disabled/st_active_nokhte_sessions/queries.dart
@@ -1,5 +1,8 @@
 // ignore_for_file: constant_identifier_names
 import 'package:nokhte_backend/edge_functions/active_nokhte_session.dart';
+import 'package:nokhte_backend/tables/company_presets.dart';
+import 'package:nokhte_backend/tables/rt_active_nokhte_sessions.dart';
+import 'package:nokhte_backend/tables/user_information.dart';
 import 'package:nokhte_backend/types/types.dart';
 import 'package:nokhte_backend/utils/utils.dart';
 import 'constants.dart';
@@ -7,9 +10,15 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 class STActiveNokhteSessionQueries extends ActiveNokhteSessionEdgeFunctions
     with STActiveNokhteSessionsConstants, SessionUtils {
+  final UserInformationQueries userInformationQueries;
+  final RTActiveNokhteSessionQueries realtimeQueries;
+  final CompanyPresetsQueries companyPresetsQueries;
+
   STActiveNokhteSessionQueries({
     required super.supabase,
-  });
+  })  : userInformationQueries = UserInformationQueries(supabase: supabase),
+        companyPresetsQueries = CompanyPresetsQueries(supabase: supabase),
+        realtimeQueries = RTActiveNokhteSessionQueries(supabase: supabase);
 
   Future<List> select() async => await supabase.from(TABLE).select();
 
@@ -47,6 +56,62 @@ class STActiveNokhteSessionQueries extends ActiveNokhteSessionEdgeFunctions
             VERSION: currentVersion + 1,
           }),
           version: currentVersion,
+        );
+      },
+      shouldRetry: (result) {
+        return result.isEmpty;
+      },
+    );
+  }
+
+  Future<List> initializeSession({
+    PresetTypes presetType = PresetTypes.none,
+  }) async {
+    return await retry<List>(action: () async {
+      final fullName = await userInformationQueries.getFullName();
+      String preferredPreset = '';
+      if (presetType == PresetTypes.none) {
+        preferredPreset =
+            await userInformationQueries.getPreferredPresetUID() ?? '';
+      } else {
+        preferredPreset = (await companyPresetsQueries.select(
+          type: presetType,
+        ))
+            .first[CompanyPresetsQueries.UID];
+      }
+
+      if (fullName.isEmpty || preferredPreset.isEmpty) return [];
+
+      final staticRes = await supabase.from(TABLE).insert({
+        COLLABORATOR_UIDS: [userUID],
+        COLLABORATOR_NAMES: [fullName],
+        PRESET_UID: preferredPreset,
+      }).select();
+
+      if (staticRes.isEmpty) return [];
+
+      final sessionUID = staticRes.first[SESSION_UID];
+
+      return await supabase.from(realtimeQueries.TABLE).insert({
+        realtimeQueries.SESSION_UID: sessionUID,
+      }).select();
+    }, shouldRetry: (result) {
+      return result.isEmpty;
+    });
+  }
+
+  Future<List> updateSessionType(String newPresetUID) async {
+    await computeCollaboratorInformation();
+    final res = await getCreatedAt();
+    final version = res.currentVersion;
+    return await retry<List>(
+      action: () async {
+        return await _onCurrentActiveNokhteSession(
+          supabase.from(TABLE).update({
+            PRESET_UID: newPresetUID,
+            VERSION: version + 1,
+          }),
+          version: version,
         );
       },
       shouldRetry: (result) {
