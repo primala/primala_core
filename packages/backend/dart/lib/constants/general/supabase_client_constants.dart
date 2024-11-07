44aa3c3029fe2f3b2e29e76155e4c9a7a0f7c4c9
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-11-07 11:52:24 -0600

    ci: try this

--- packages/backend/dart/lib/constants/general/supabase_client_constants.dart
+++ packages/backend/dart/lib/constants/general/supabase_client_constants.dart
@@ -6,6 +6,8 @@ class SupabaseClientConfigConstants {
   static SupabaseClient get supabase => SupabaseClient(
         SupabaseConfigConstants.apiUrl,
         SupabaseConfigConstants.anonKey,
+        authOptions:
+            const FlutterAuthClientOptions(authFlowType: AuthFlowType.implicit),
       );
   static SupabaseClient get supabaseAdmin => SupabaseClient(
         SupabaseConfigConstants.apiUrl,
