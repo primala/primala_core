9ef830f30dd002406a9df355e4bf11cebded0436
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-12-22 07:48:29 -0600

    bump app version

--- packages/backend/supabase/migrations/20241218205944_add_requests_and_collaborator_list.sql
+++ packages/backend/supabase/migrations/20241218205944_add_requests_and_collaborator_list.sql
@@ -297,3 +297,13 @@ alter table "public"."collaborator_relationships" alter column "uid" set default
 
 alter table public.collaborator_requests
 add constraint unique_sender_recipient unique (sender_uid, recipient_uid);
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.get_valid_app_versions()
+ RETURNS text[]
+ LANGUAGE plpgsql
+AS $function$BEGIN 
+    RETURN ARRAY['2.44.0'];
+END;$function$
+;
