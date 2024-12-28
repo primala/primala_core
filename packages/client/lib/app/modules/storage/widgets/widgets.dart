6177321685687fcd2eccbfbeabe1638620d4b021
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-12-28 07:43:12 -0600

    remove old session card implementation

--- packages/client/lib/app/modules/storage/widgets/widgets.dart
+++ packages/client/lib/app/modules/storage/widgets/widgets.dart
@@ -1,5 +1,4 @@
 export 'content_card/content_card.dart';
-export 'session_card/session_card.dart';
 export 'group_display/group_display.dart';
 export 'group_registration/group_registration.dart';
 export 'group_display_modal/group_display_modal.dart';
