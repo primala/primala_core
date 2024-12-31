d9add107c2c561da28b9c2d7e6dedaac6bde0487
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2024-12-31 12:03:07 -0600

    add session content binds

--- packages/client/lib/app/core/modules/session_content/session_content.dart
+++ packages/client/lib/app/core/modules/session_content/session_content.dart
@@ -1,2 +1,32 @@
 export 'domain/domain.dart';
 export 'data/data.dart';
+export 'widgets/widgets.dart';
+
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:nokhte/app/core/network/network_info.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+import 'session_content.dart';
+
+class SessionContentModule extends Module {
+  @override
+  void exportedBinds(Injector i) {
+    i.add<SessionContentRemoteSourceImpl>(
+      () => SessionContentRemoteSourceImpl(
+        supabase: Modular.get<SupabaseClient>(),
+      ),
+    );
+    i.add<SessionContentContractImpl>(
+      () => SessionContentContractImpl(
+        remoteSource: i<SessionContentRemoteSourceImpl>(),
+        networkInfo: Modular.get<NetworkInfoImpl>(),
+      ),
+    );
+
+    i.add<SessionContentLogicCoordinator>(
+      () => SessionContentLogicCoordinator(
+        contract: i<SessionContentContractImpl>(),
+      ),
+    );
+  }
+}
