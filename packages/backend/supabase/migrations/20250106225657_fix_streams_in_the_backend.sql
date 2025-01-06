1ca9b7752eeaf25861ea0b046c506f4de18b7764
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2025-01-06 17:00:10 -0600

    fix no updates on delete for select tables

---
+++ packages/backend/supabase/migrations/20250106225657_fix_streams_in_the_backend.sql
@@ -0,0 +1,3 @@
+alter table "public"."session_content" replica identity full;
+alter table "public"."session_information" replica identity full;
+alter table "public"."group_information" replica identity full;
