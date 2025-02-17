d4e15a4b5444a69cd6dbafd9189f326d0f04e8b7
Sonny Vesali <79886185+sonnyvesali@users.noreply.github.com>
2025-02-17 16:42:05 -0600

    refactor auth utils into contract utils

--- packages/client/lib/app/core/utilities/contract_utils.dart
+++ packages/client/lib/app/core/utilities/contract_utils.dart
@@ -4,27 +4,24 @@ import 'package:nokhte/app/core/error/failure.dart';
 import 'package:nokhte/app/core/network/network_info.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-mixin AuthUtils {
+mixin ContractUtils {
   Future<Either<Failure, T>> handleRemoteOperation<T>({
     required Future<T> Function() operation,
     required NetworkInfo networkInfo,
+    required Failure failure,
   }) async {
     if (await networkInfo.isConnected) {
       try {
         final result = await operation();
         return Right(result);
       } catch (e) {
-        // You can add more specific error handling here based on the error type
         if (e is AuthException) {
-          return Left(AuthFailure(
-            message: e.message,
-            failureCode: e.toString(),
-          ));
+          return Left(failure);
         }
-        return Left(FailureConstants.authFailure);
+        return Left(failure);
       }
     } else {
-      return Left(FailureConstants.internetConnectionFailure);
+      return const Left(FailureConstants.internetConnectionFailure);
     }
   }
 }
